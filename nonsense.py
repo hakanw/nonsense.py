6c63642acd39f65c1a667634eaa44461c4095caa
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2013-12-05 17:00:32 +0100

    Just a bunch of whitespace fixes.

--- nonsense.py
+++ nonsense.py
@@ -24,16 +24,16 @@ def stderr(str):
 class MarkovChain(object):
     def __init__(self, input_file=None, lookback=3, no_cache=False):
         self.WORD_RE = re.compile(r"([\w\.\!\,]+)", re.UNICODE)
-        self.NO_REAL_WORD_RE = re.compile(r'^[\d\.\,\:\;]*$')
+        self.NO_REAL_WORD_RE = re.compile(r"^[\d\.\,\:\;]*$")
         self.lookback = lookback
-        
+
         if no_cache:
             self.conn = sqlite3.connect(":memory:")
         else:
             self.conn = sqlite3.connect("%s.markovdb~%d" % (input_file, lookback))
-        
+
         self.c = self.conn.cursor()
-        
+
         if input_file:
             self.input(input_file)
 
@@ -45,41 +45,41 @@ class MarkovChain(object):
 
             with open(input_file) as file:
                 self.generate_markov_chain(file.read().decode("utf-8"))
-        
+
     def init_database(self):
        self.c.execute("CREATE TABLE IF NOT EXISTS markov_chain (prefix text, suffix text, num_occurences integer DEFAULT 0, probability real, UNIQUE (prefix, suffix) )");
 
-    def generate_markov_chain(self, input):        
+    def generate_markov_chain(self, input):
         # interface to the db
         def add_suffix(prefix, suffix):
             self.c.execute("INSERT OR IGNORE INTO markov_chain (prefix, suffix) VALUES (?, ?)", (prefix, suffix))
             self.c.execute("UPDATE markov_chain SET num_occurences = num_occurences + 1 WHERE prefix=? AND suffix=?", (prefix, suffix))
-        
+
         # estimate num words so we can give progress
         word_count_estimate = len(input.split(" "))
 
-        i=0
+        i = 0
         prev_prefixes = []
         for match in re.finditer(self.WORD_RE, input):
             word = match.groups()[0].lower()
             if not re.match(self.NO_REAL_WORD_RE, word):
-                i+=1
-                if i % (word_count_estimate/10) == 0:
-                    print "%d%%" % int(i/float(word_count_estimate) * 100.0)
-                
+                i += 1
+                if i % (word_count_estimate / 10) == 0:
+                    stderr("%d%%" % int(i / float(word_count_estimate) * 100.0))
+
                 # add all prefixes => this word tuples
                 for prefix in prev_prefixes:
                     add_suffix(prefix, word)
-                    
+
                 # new meaning-prefix (if last word ended in dot)
                 if prev_prefixes and prev_prefixes[-1][-1] == ".":
                     add_suffix("^", word)
-                    
+
                 # generate new prefixes
                 if len(prev_prefixes) >= self.lookback:
                     # remove longest prefix and add current word to all of the remaining prefixes
                     prev_prefixes.pop(0)
-                
+
                 temp = map(lambda prefix: prefix + " " + word, prev_prefixes)
                 prev_prefixes = temp
                 prev_prefixes.append(word)
@@ -93,59 +93,59 @@ class MarkovChain(object):
             self.c.execute("UPDATE markov_chain SET probability=((1.0*num_occurences)/?) WHERE prefix=?", (float(row[1]), row[0]))
 
         self.conn.commit()
-            
+
     def choose_next_word(self, from_prefix):
         random_choice = random.random()
-        i=0
+        i = 0
         for row in self.c.execute("SELECT suffix, probability FROM markov_chain WHERE prefix=? ORDER BY RANDOM()", (from_prefix,)):
-           i+=row[1] 
-           if i>=random_choice:
+           i += row[1]
+           if i >= random_choice:
                return row[0]
-        
+
     def generate_sentence(self, start_word=None, min_words=5, max_length=140, prevent_recursion=False):
         first_word = None
         if start_word:
             first_word = self.choose_next_word(start_word.lower())
-        
+
         if first_word:
             word_queue = [start_word, first_word]
             out = start_word.capitalize() + " " + first_word
-        else: 
+        else:
             first_word = self.choose_next_word(start_word or "^")
             word_queue = [first_word]
             out = first_word.capitalize()
-        
+
         while word_queue[-1].find(".") == -1:
             suggestion = None
-            
+
             # randomize how many words prefix we should start trying at
-            for num_words_to_try in reversed(range(1, random.randint(1, self.lookback)+1)):
+            for num_words_to_try in reversed(range(1, random.randint(1, self.lookback) + 1)):
                 if len(word_queue) >= num_words_to_try:
                     prefix = " ".join(word_queue[-num_words_to_try:])
                     suggestion = self.choose_next_word(prefix)
                     if suggestion:
                         break
-                    
+
 
             if not suggestion:
                 break
-        
+
             word_queue.append(suggestion)
-        
+
         out = " ".join(word_queue)
-            
+
         if len(word_queue) < min_words or len(out) > max_length and not prevent_recursion:
             # hack: re-try on average 10 times if we have bad outputs :)
             stderr("Output not ideal... retrying with another run. (max 10 times)")
-            out = self.generate_sentence(prevent_recursion=(random.random()*10 < 1))
-        
+            out = self.generate_sentence(prevent_recursion=(random.random() * 10 < 1))
+
         return out
-    
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         stderr("Usage: ./nonsense.py [--lookback N] [--startword Hello] [--min-length 20] [--max-length 300] [--no-cache] [--only-cache] input_file_with_lotsa_words.txt")
         sys.exit(1)
-    
+
     # default arguments
     start_word = None
     max_length = 140
@@ -155,7 +155,7 @@ if __name__ == "__main__":
 
     # ugly but simple enough argument handling
     args = sys.argv[1:]
-    
+
     if "--startword" in args:
         i = args.index("--startword")
         start_word = args[i+1].decode("utf-8")
